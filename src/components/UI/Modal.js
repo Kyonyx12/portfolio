a43507a578b8c772a77923ce281f73c542d9736c
Kyonyx12 <themisteryofworld@gmail.com>
2021-05-07 01:09:59 -0700

    modal upgrade

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,32 +1,36 @@
-import { RiCloseCircleLine } from "react-icons/ri";
+import {
+  RiCloseCircleLine,
+  RiGithubFill,
+  RiExternalLinkFill,
+} from "react-icons/ri";
 import "./Modal.css";
 
 export default function Modal({ selected, handleModal }) {
   const { project, site, code, img } = selected;
 
   return (
-    <div className="modal">
+    <div className="modal" onClick={() => handleModal()}>
       <div className="modal-info">
         <button onClick={() => handleModal()} className="modal-close">
           <RiCloseCircleLine className="modal-close-icon" color="#00b0f0" />
         </button>
         <img src={img} alt={project} className="modal-img" />
-        <div>
+        <div className="modal-link-container">
           <a
-            className="modal-button"
+            className="modal-link"
             href={site}
             target="_blank"
             rel="noreferrer"
           >
-            <p>See live</p>
+            <RiExternalLinkFill size="2.5rem" className="modal-link-icon" />
           </a>
           <a
-            className="modal-button"
+            className="modal-link"
             href={code}
             target="_blank"
             rel="noreferrer"
           >
-            <p>See on Github</p>
+            <RiGithubFill size="2.5rem" className="modal-link-icon" />
           </a>
         </div>
       </div>
