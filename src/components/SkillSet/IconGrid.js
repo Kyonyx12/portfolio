438818ccdc0f9b733302b2505588081ded7432c6
Kyonyx12 <themisteryofworld@gmail.com>
2021-05-07 19:23:27 -0700

    favicon added

--- src/components/SkillSet/IconGrid.js
+++ src/components/SkillSet/IconGrid.js
@@ -6,51 +6,59 @@ import {
   SiJavascript,
   SiGit,
   SiNpm,
-  SiGithub
+  SiGithub,
 } from "react-icons/si";
 
 export default function IconGrid() {
   return (
     <div className="flex-icons-container">
       <div className="icon-bg">
-        <div className="icon">
-          <SiHtml5 size="5rem" color="#f47532" />
+        <div className="icon alt">
+          <SiHtml5 size="5rem" color="#f47532" title="HTML5" />
+          <span>HTML</span>
         </div>
       </div>
       <div className="icon-bg">
-        <div className="icon">
-          <SiCss3 size="5rem" color="#2a9aca" />
+        <div className="icon alt">
+          <SiCss3 size="5rem" color="#2a9aca" title="CSS3" />
+          <span>CSS3</span>
         </div>
       </div>
       <div className="icon-bg">
-        <div className="icon">
-          <SiBootstrap size="5rem" color="#5d3c7f" />
+        <div className="icon alt">
+          <SiBootstrap size="5rem" color="#5d3c7f" title="Bootstrap" />
+          <span>Bootstrap</span>
         </div>
       </div>
       <div className="icon-bg">
-        <div className="icon">
-          <SiJavascript size="5rem" color="#f7df1e" />
+        <div className="icon alt">
+          <SiJavascript size="5rem" color="#f7df1e" title="JavaScript" />
+          <span>JavaScript</span>
         </div>
       </div>
       <div className="icon-bg">
-        <div className="icon">
-          <SiReact size="5rem" color="#61dbfb" />
+        <div className="icon alt">
+          <SiReact size="5rem" color="#61dbfb" title="React" />
+          <span>React</span>
         </div>
       </div>
       <div className="icon-bg">
-        <div className="icon">
-          <SiGit size="5rem" color="#f05033" />
+        <div className="icon alt">
+          <SiGit size="5rem" color="#f05033" title="Git" />
+          <span>Git</span>
         </div>
       </div>
 
       <div className="icon-bg">
-        <div className="icon">
-          <SiGithub size="5rem" color="#191919" />
+        <div className="icon alt">
+          <SiGithub size="5rem" color="#191919" title="Github" />
+          <span>GitHub</span>
         </div>
       </div>
       <div className="icon-bg">
-        <div className="icon">
-          <SiNpm size="5rem" color="#cc3534" />
+        <div className="icon alt">
+          <SiNpm size="5rem" color="#cc3534" title="NPM" />
+          <span>NPM</span>
         </div>
       </div>
     </div>
