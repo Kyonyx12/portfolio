d821530daf3cfcc4bc76d74e29c1db3671275a07
Kyonyx12 <themisteryofworld@gmail.com>
2021-05-09 01:28:11 -0700

    form fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
 import Main from "./components/Main";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function App() {
   return (
     <div className="App">
       <Main />
+      <ToastContainer />
     </div>
   );
 }
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,25 +1,29 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
+import "./Contact.css";
+
+const initialState = {
+  nameKey: "entry.115349326",
+  emailKey: "entry.1480087315",
+  messageKey: "entry.434096257",
+};
 
 export default function Contact() {
   const [submitted, setSubmitted] = useState(false);
-  const [form, setForm] = useState({
-    "entry.115349326": "",
-    "entry.1480087315": "",
-    "entry.434096257": "",
-  });
+  const [form, setForm] = useState(initialState);
 
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  };
+  // const handleChange = (e) => {
+  //   setForm({ ...form, [e.target.name]: e.target.value });
+  // };
+
   const handleSubmit = (e) => {
-    console.log("Submitted!");
+    setForm(initialState);
     setSubmitted(!submitted);
+    toast("Contact info submitted!", {
+      type: "info",
+    });
   };
-  const handleLoad = () => {
-    if (submitted) {
-      window.location = "https://joseantoniolopezramos.site/";
-    }
-  };
+
   return (
     <section className="contact">
       <div>
@@ -27,7 +31,6 @@ export default function Contact() {
         <hr />
       </div>
       <form
-        method="post"
         target="hidden_iframe"
         onSubmit={handleSubmit}
         encType="text/plain"
@@ -35,39 +38,31 @@ export default function Contact() {
       >
         <div className="form-name">
           <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="entry.115349326"
-            name="entry.115349326"
-            value={form.name}
-            onChange={handleChange}
-            required
-          />
+          <input type="text" id={form.nameKey} name={form.nameKey} required />
         </div>
-
         <div className="form-name">
           <label htmlFor="email">Email</label>
           <input
             type="email"
-            id="entry.1480087315"
-            name="entry.1480087315"
-            value={form.email}
-            onChange={handleChange}
+            id={form.emailKey}
+            name={form.emailKey}
             required
           />
         </div>
         <div className="form-name">
           <label htmlFor="message">Message</label>
-          <input
+          <textarea
             type="text"
-            id="entry.434096257"
-            name="entry.434096257"
-            value={form.message}
-            onChange={handleChange}
+            id={form.messageKey}
+            name={form.messageKey}
             required
           />
         </div>
-        <button className="send" type="submit" href="#index.html">
+        <button
+          className={` ${submitted ? "disabled" : "enabled"}`}
+          type="submit"
+          disabled={submitted && true}
+        >
           Send
         </button>
       </form>
@@ -76,7 +71,6 @@ export default function Contact() {
         name="hidden_iframe"
         id="hidden_iframe"
         style={{ display: "none" }}
-        onLoad={handleLoad}
       ></iframe>
     </section>
   );
