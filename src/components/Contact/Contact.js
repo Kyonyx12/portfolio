d941fac8cc90006db88e1b36c34b48541d421007
Kyonyx12 <themisteryofworld@gmail.com>
2021-05-08 23:16:38 -0700

    form improved

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,22 +1,25 @@
 import { useState } from "react";
 
 export default function Contact() {
+  const [submitted, setSubmitted] = useState(false);
   const [form, setForm] = useState({
-    name: "",
-    email: "",
-    phone: "",
-    message: "",
-    sum: "",
+    "entry.115349326": "",
+    "entry.1480087315": "",
+    "entry.434096257": "",
   });
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   alert("El formulario se ha enviado!");
-  // };
-
+  const handleSubmit = (e) => {
+    console.log("Submitted!");
+    setSubmitted(!submitted);
+  };
+  const handleLoad = () => {
+    if (submitted) {
+      window.location = "https://joseantoniolopezramos.site/";
+    }
+  };
   return (
     <section className="contact">
       <div>
@@ -25,14 +28,17 @@ export default function Contact() {
       </div>
       <form
         method="post"
+        target="hidden_iframe"
+        onSubmit={handleSubmit}
         encType="text/plain"
-        action="mailto:isselocreyo12@yopmail.com"
+        action="https://docs.google.com/forms/d/e/1FAIpQLSexF0uIO68Gq9zQ5qlvo0Nc924FukaKRuQqy0NpmRHJkYbYtg/formResponse?"
       >
         <div className="form-name">
           <label htmlFor="name">Name</label>
           <input
-            id="name"
-            name="name"
+            type="text"
+            id="entry.115349326"
+            name="entry.115349326"
             value={form.name}
             onChange={handleChange}
             required
@@ -43,20 +49,9 @@ export default function Contact() {
           <label htmlFor="email">Email</label>
           <input
             type="email"
-            id="email"
-            name="email"
+            id="entry.1480087315"
+            name="entry.1480087315"
             value={form.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className="form-name">
-          <label htmlFor="phone">Phone</label>
-          <input
-            type="number"
-            id="phone"
-            name="phone"
-            value={form.phone}
             onChange={handleChange}
             required
           />
@@ -64,18 +59,25 @@ export default function Contact() {
         <div className="form-name">
           <label htmlFor="message">Message</label>
           <input
-            id="message"
-            name="message"
+            type="text"
+            id="entry.434096257"
+            name="entry.434096257"
             value={form.message}
             onChange={handleChange}
             required
           />
         </div>
-
-        <button className="send" type="submit">
+        <button className="send" type="submit" href="#index.html">
           Send
         </button>
       </form>
+      <iframe
+        title="hidden_iframe"
+        name="hidden_iframe"
+        id="hidden_iframe"
+        style={{ display: "none" }}
+        onLoad={handleLoad}
+      ></iframe>
     </section>
   );
 }
